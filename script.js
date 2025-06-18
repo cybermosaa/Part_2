641dd177c33037d4e5df4b855a80938684cb5c63
cybermosaa <ST10485614@rcconnect.edu.za>
2025-06-18 13:04:00 +0200

    added back to top button eleemnts

--- script.js
+++ script.js
@@ -76,22 +76,21 @@ function initSearch(){
   })
 }
 
-// // Back to top button 
-// function initBackToTop() {
-//     const backToTopBtn = document.getElementById('backToTop');
-    
-//     window.addEventListener('scroll', () => {
-//         if (window.pageYOffset > 300) {
-//             backToTopBtn.style.display = 'block';
-//         } else {
-//             backToTopBtn.style.display = 'none';
-//         }
-//     });
-    
-//     backToTopBtn.addEventListener('click', () => {
-//         window.scrollTo({ top: 0, behavior: 'smooth' });
-//     });
-// }
+// Back to top button 
+function initBackToTop(){
+  const backToTopBtn = document.getElementById('backToTopBtn');
+  window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 300){
+      backToTopBtn.style.display = 'block';
+    } else {
+      backToTopBtn.style.display = 'none';
+    }
+  });
+
+  backToTopBtn.addEventListener('click', () => {
+   window.scrollTo({ top: 0, behavior: 'smooth' });
+  })
+}
 
 // Form Validation for Contact & Enquiry
 function validateForm(formId) {
